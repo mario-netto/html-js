3b85d18ee5d04fc3fe9aaa76c661be29c9480225
Mario Netto <marionettojr@gmail.com>
2024-11-17 12:03:01 -0300

    att js

--- main.js
+++ main.js
@@ -1,45 +1,67 @@
-const form = document.getElementById('form-valores');
+//Codigo atualizado
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    const valorA = Number(document.getElementById('idCampoA').value);
-    const valorB = Number (document.getElementById('idCampoB').value);
+document.addEventListener('DOMContentLoaded', function() {
+    const formValor = document.querySelector('#form-valores')
+    const valorA = document.querySelector('#idCampoA')
+    const valorB = document.querySelector('#idCampoB')
+    const successMessage = document.querySelector('.success-message')
+    const errorMessage = document.querySelector('.error-message')
 
-    const valorA2 = document.getElementById('idCampoA');
-    const valorB2 = document.getElementById('idCampoB');
+    formValor.addEventListener('submit', function(e) {
+        e.preventDefault()
 
-    const msgSucesso = `O valor do campo <strong>B=${valorB}</strong> é maior que valor do campo <strong>A=${valorA}</strong>`;
-    const msgErro = `O valor do campo <strong>B=${valorB}</strong> é menor que valor do campo <strong>A=${valorA}</strong>`;
+        if (valorB.value > valorA.value) {
+            successMessage.style.display = 'block'
+            successMessage.innerHTML = `O valor do campo <strong>B = ${valorB.value}</strong> é maior que valor do campo <strong>A = ${valorA.value}</strong>`
+        } else {
+            errorMessage.style.display = 'block'
+            errorMessage.innerHTML = `O valor do campo <strong>B = ${valorB.value}</strong> é menor que valor do campo <strong>A = ${valorA.value}</strong>`
+        }
+    })
+})
 
-    let formTeste = false;
+//codigo antigo
 
-    function valor(valorA, valorB){
-        return valorA < valorB;
-    }
+// const form = document.getElementById('form-valores');
 
-    const mS = document.querySelector('.success-message');
-    const mE = document.querySelector('.error-message');
+// form.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     const valorA = Number(document.getElementById('idCampoA').value);
+//     const valorB = Number (document.getElementById('idCampoB').value);
 
-    formTeste = valor(valorA, valorB);
+//     const valorA2 = document.getElementById('idCampoA');
+//     const valorB2 = document.getElementById('idCampoB');
 
-    if (formTeste){
-        mS.innerHTML = msgSucesso;
-        mS.style.display = 'block';
-        mE.style.display = 'none';
+//     const msgSucesso = `O valor do campo <strong>B=${valorB}</strong> é maior que valor do campo <strong>A=${valorA}</strong>`;
+//     const msgErro = `O valor do campo <strong>B=${valorB}</strong> é menor que valor do campo <strong>A=${valorA}</strong>`;
 
-        valorA2.style.border = '';
-        mE.innerHTML = '';
+//     let formTeste = false;
 
-        valorA2.value = '';
-        valorB2.value = '';
-    } else {
-        valorA2.style.border = '1px solid red';
-        mE.innerHTML = msgErro;
-        mE.style.display = 'block';
-        mS.style.display = 'none';
+//     function valor(valorA, valorB){
+//         return valorA < valorB;
+//     }
 
-        valorB2.value = '';
-    }
-    
+//     const mS = document.querySelector('.success-message');
+//     const mE = document.querySelector('.error-message');
 
-});
+//     formTeste = valor(valorA, valorB);
+
+//     if (formTeste){
+//         mS.innerHTML = msgSucesso;
+//         mS.style.display = 'block';
+//         mE.style.display = 'none';
+
+//         valorA2.style.border = '';
+//         mE.innerHTML = '';
+
+//         valorA2.value = '';
+//         valorB2.value = '';
+//     } else {
+//         valorA2.style.border = '1px solid red';
+//         mE.innerHTML = msgErro;
+//         mE.style.display = 'block';
+//         mS.style.display = 'none';
+
+//         valorB2.value = '';
+//     }
+// });
